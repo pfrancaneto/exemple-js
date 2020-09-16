c5eb7e750e6f8e0eef7000a9f0cf5d1c08dd3d08
pfrancaneto <34045576+pfrancaneto@users.noreply.github.com>
2020-09-16 14:16:44 -0300

    server node js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
-
 const server = express();
+
+server
+.use(express.static("public"))
+
+.get('/', (req, res) => {
+  return res.sendFile(__dirname + "/views/index.html")
+})
+
+.get('/classes', (req, res) => {
+  return res.sendFile(__dirname + "/views/classes.html")
+})
+  
+.listen(3300, () => {
+  console.log('🚀 ⛱ server running'); 
+})
